2aeb5b5d1a0b29028a1e3ea9b48e1c6ca2a751bd
FOZILJONOV NURIDDIN <foziljonovnuriddin04@gmail.com>
2025-06-28 16:48:28 +0500

    hulas

--- app/src/main/java/com/example/food/MainActivity.kt
+++ app/src/main/java/com/example/food/MainActivity.kt
@@ -1,26 +1,25 @@
 package com.example.food
 
-import android.R.attr.fragment
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import com.example.food.Fragment.Fragment_1
-import com.example.food.Fragment.Fragment_login
+import androidx.navigation.ui.NavigationUI
 import com.example.food.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
+        // BottomNavigationView ni NavController bilan bog‘lash
+        val navController = findNavController(R.id.homev1Fragment)
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
     }
 
+    // Back tugmasi bosilganda orqaga navigatsiya qilish uchun
     override fun onSupportNavigateUp(): Boolean =
-        findNavController(R.id.navigation).navigateUp()
-
+        findNavController(R.id.homev1Fragment).navigateUp()
 }
